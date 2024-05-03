290d3e99e483b2a6fa118ea5cc6587cb5160bd8f
Bianca Barros <biancarbarrosz@gmail.com>
2024-05-03 10:03:29 -0300

    first commit

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo! :). Esse é meu primeiro programa em java!");
+	}
+
+}
