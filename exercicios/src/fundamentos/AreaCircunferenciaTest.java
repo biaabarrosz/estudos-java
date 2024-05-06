658745663d809b16b279ec7fa62696ccd149ef94
Bianca Barros <biancarbarrosz@gmail.com>
2024-05-06 15:13:55 -0300

    mais conteúdos

---
+++ exercicios/src/fundamentos/AreaCircunferenciaTest.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AreaCircunferenciaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
