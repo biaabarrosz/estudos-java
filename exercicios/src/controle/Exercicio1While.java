97f2ea956fb287e296e1fba2d38be044b1cc279b
Bianca Barros <biancarbarrosz@gmail.com>
2024-05-08 15:45:57 -0300

    questões WHILE e FOR

---
+++ exercicios/src/controle/Exercicio1While.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class Exercicio1While {
+	
+	public static void main(String[] args) {
+		
+		for(int contador = 10; contador >= 1; contador--) {
+		System.out.print(contador);
+		}
+		}
+
+}
--- exercicios/src/controle/For3.java
+++ exercicios/src/controle/For3.java
@@ -2,4 +2,7 @@ package controle;
 
 public class For3 {
 
+	public static void main(String[] args) {
+		
+	}
 }
